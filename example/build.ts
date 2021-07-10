e3ee9ea1e6a52d72dde8782bb6c83a1999804790
eibens <l.eibensteiner@gmail.com>
2021-07-10 12:05:03 +0200

    docs: improve example

--- example/build.ts
+++ example/build.ts
@@ -1,7 +1,19 @@
-import { broadcast, bundle, glob, renderHtml, rule, watch } from "../mod.ts";
+// Import the surv DSL.
+import {
+  broadcast,
+  bundle,
+  deployctl,
+  glob,
+  kind,
+  renderHtml,
+  rule,
+  watch,
+} from "../mod.ts";
 
+// Ensure the output dir exists.
 await Deno.mkdir("docs", { recursive: true });
 
+// Generate the main HTML file once.
 await Deno.writeTextFile(
   "docs/index.html",
   renderHtml({
@@ -9,6 +21,24 @@ await Deno.writeTextFile(
   }),
 );
 
+// Start the HTTP server.
+// NOTE: This should be replaced, once process restarting is fixed.
+deployctl("serve.js")();
+
+// Restart the HTTP server whenever server.js changes.
+// FIXME: Server is restarted before it is closed.
+// This leads to address already being in use.
+// Ideally, the `start` would ensure the process is terminated
+// before restarting the process.
+//watch(
+//  "serve.js",
+//  rule(
+//    kind("modify"),
+//    deployctl("serve.js"),
+//  ),
+//);
+
+// Send a message to websocket clients files if `docs` change.
 watch(
   "docs",
   broadcast({
@@ -16,6 +46,7 @@ watch(
   }),
 );
 
+// Bundle the main script file when TypeScript files change.
 watch(
   ".",
   rule(
--- example/index.ts
+++ example/index.ts
@@ -1,10 +1,14 @@
+// This triple-slash directive is needed to use the DOM.
 /// <reference lib="dom"/>
 
+// Reload the page whenever a WebSocket message is received.
 const ws = new WebSocket("ws://localhost:1234");
 ws.addEventListener("message", () => {
   window.location.reload();
 });
 
+// Generate HTML.
+// While `build.ts` is running, changes will trigger a page reload.
 document.body.innerHTML = `
 <h1>Website</h1>
 `;
