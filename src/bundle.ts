d0c70dae0a27d7c883d51f70799e3ee4c48c067a
eibens <l.eibensteiner@gmail.com>
2021-07-21 23:54:34 +0200

    fix(bundler): actually use tsconfig option

--- src/bundle.ts
+++ src/bundle.ts
@@ -1,12 +1,15 @@
 import { run } from "./process.ts";
 
-export function bundle(src: string, out: string) {
+export function bundle(src: string, out: string, options: {
+  tsconfig?: string;
+} = {}) {
   // Create JavaScript bundle.
   return run({
     cmd: [
       "deno",
       "bundle",
       "--no-check", // DOM use causes type errors
+      ...(options.tsconfig ? ["--config=" + options.tsconfig] : []),
       src,
       out,
     ],
